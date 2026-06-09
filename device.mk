cfaaad46c84ea7caadd1dbdd62def4287e5bc209
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-09 07:53:36 -0400

    j5xnlte: Move system_prop values to j5x-common
    The values that were in system_prop.mk are common amongst all of the
    j5 2016 devices. It would be more appropriate to have them moved to
    j5x-common. Also, drop zram property as this is disabled by default.

--- device.mk
+++ device.mk
@@ -6,8 +6,5 @@ $(call inherit-product, device/samsung/j5x-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/j5xnlte
 
-# System properties
--include $(LOCAL_PATH)/system_prop.mk
-
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
